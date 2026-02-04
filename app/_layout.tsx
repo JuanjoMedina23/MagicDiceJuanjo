89a53c425875c2f9b8a75fc61ae6baaf42a70eae
jjmmhola2 <jjmmhola2@gmail.com>
2026-02-04 20:21:44 +0000

    ✨ (feat): Sesion Global     -Se envuelve el push de notification que hace saber a toda la app que      hay una sesion iniciada y le da el id de esa sesion.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,19 @@
 import { Stack } from 'expo-router';
-import { AuthProvider } from '../lib/modules/auth/AuthProvider';
+import { AuthProvider, useAuth } from '../lib/modules/auth/AuthProvider';
+import { usePushNotifications } from '@/lib/modules/notifications/usePushNotifications';
+
+function AuthLayout() {
+  const {session} = useAuth();
+  const userId = session?.user.id;
+  usePushNotifications(userId);
+  return <Stack/>;
+}
 
 export default function RootLayout() {
   return (
     <AuthProvider>
       <Stack screenOptions={{ headerShown: false }} /> {/* El resto de tu app */}
+      <AuthLayout />
     </AuthProvider>
   );
 }
